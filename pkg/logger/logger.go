a4ee418a6ded44724d6bed7d333fdfb7817b3c43
Emirlan Rasulov <rasulov.emirlan@gmail.com>
2022-04-19 00:25:10 +0600

    added logger config

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,19 +1,24 @@
 package logger
 
 import (
+	"os"
+
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func NewZap(filename string, isDev bool, level zap.AtomicLevel) (*zap.SugaredLogger, error) {
-	cfg := zap.Config{
-		Level:       level,
-		Development: isDev,
-		Encoding:    "console",
-		OutputPaths: []string{filename},
-	}
-	z, err := cfg.Build()
+	conf := zap.NewProductionEncoderConfig()
+	conf.EncodeTime = zapcore.ISO8601TimeEncoder
+	fileencoder := zapcore.NewJSONEncoder(conf)
+	logFile, err := os.OpenFile(filename, os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
 	if err != nil {
 		return nil, err
 	}
-	return z.Sugar(), nil
+	writer := zapcore.AddSync(logFile)
+	core := zapcore.NewTee(
+		zapcore.NewCore(fileencoder, writer, level),
+	)
+	l := zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+	return l.Sugar(), nil
 }
